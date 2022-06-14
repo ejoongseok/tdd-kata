525119b260267c30926d409581a8dbc530772120
ejoongseok <ejoongseok@gmail.com>
2022-06-14 10:30:11 +0900

    스페어 처리

--- src/main/java/bowling/Game.java
+++ src/main/java/bowling/Game.java
@@ -1,12 +1,22 @@
 package bowling;
 
+import java.util.Arrays;
+
 public class Game {
-	int score = 0;
+	int[] score = new int[22];
+	int scoreCount = 1;
 	public void roll(int pinOverCount) {
-		score += pinOverCount;
+		score[scoreCount++] = pinOverCount;
 	}
 
 	public int score() {
-		return score;
+		int result = 0;
+		for (int i = 1; i < score.length; i++) {
+			result += score[i];
+			if (i % 2 == 0 && score[i - 1] + score[i] == 10) {
+				result += score[i + 1];
+			}
+		}
+		return result;
 	}
 }
--- src/test/java/bowling/BowlingGameTest.java
+++ src/test/java/bowling/BowlingGameTest.java
@@ -3,18 +3,38 @@ package bowling;
 import static org.assertj.core.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BowlingGameTest {
+	Game game;
+	@BeforeEach
+	void setUp() {
+		game = new Game();
+	}
+
 
 	@Test
 	@DisplayName("볼링 게임")
 	void game() {
-		Game game = new Game();
-		game.roll(1);
-		game.roll(1);
+		frameFor(5, 3);
 		int score = game.score();
-		assertThat(score).isEqualTo(2);
+		assertThat(score).isEqualTo(8);
+	}
+	@Test
+	@DisplayName("볼링 게임 - 스페어")
+	void spare() {
+		frameFor(5, 5);
+		frameFor(3, 6);
+		frameFor(5, 5);
+		game.roll(3);
+		int score = game.score();
+		assertThat(score).isEqualTo(38);
+	}
+
+	void frameFor(int oneRoll, int twoRoll) {
+		game.roll(oneRoll);
+		game.roll(twoRoll);
 	}
 }
