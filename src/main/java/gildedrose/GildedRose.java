7c8e448f6f289f855cf5be6833ce4535740d39c5
ejoongseok <ejoongseok@gmail.com>
2022-06-14 15:55:50 +0900

    duplicate Method Extract

--- src/main/java/gildedrose/GildedRose.java
+++ src/main/java/gildedrose/GildedRose.java
@@ -10,52 +10,15 @@ public class GildedRose {
 	public void updateQuality() {
 		for (Item item : items) {
 			step1(item);
-
 			step2(item);
-
 			step3(item);
-
-		}
-	}
-
-	private void step3(Item item) {
-		if (item.sellIn < 0) {
-			if (!item.name.equals("Aged Brie")) {
-				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-					if (item.quality > 0) {
-						if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-							item.quality = item.quality - 1;
-						}
-					}
-				} else {
-					item.quality = item.quality - item.quality;
-				}
-			} else {
-				duplicateQualityExtract(item);
-			}
-		}
-	}
-
-	private void duplicateQualityExtract(Item item) {
-		if (item.quality < 50) {
-			item.quality = item.quality + 1;
-		}
-	}
-
-	private void step2(Item item) {
-		if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-			item.sellIn = item.sellIn - 1;
 		}
 	}
 
 	private void step1(Item item) {
 		if (!item.name.equals("Aged Brie")
 			&& !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-			if (item.quality > 0) {
-				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-					item.quality = item.quality - 1;
-				}
-			}
+			duplicatedExtract(item);
 		} else {
 			if (item.quality < 50) {
 				item.quality = item.quality + 1;
@@ -70,6 +33,39 @@ public class GildedRose {
 					}
 				}
 			}
+		}
+	}
+
+	private void step2(Item item) {
+		if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			item.sellIn = item.sellIn - 1;
+		}
+	}
+
+	private void step3(Item item) {
+		if (item.sellIn < 0) {
+			if (!item.name.equals("Aged Brie")) {
+				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+					duplicatedExtract(item);
+				} else {
+					item.quality = item.quality - item.quality;
+				}
+			} else {
+				duplicateQualityExtract(item);
+			}
+		}
+	}
+
+	private void duplicatedExtract(Item item) {
+		if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			item.quality = item.quality - 1;
+		}
+	}
+
+
+	private void duplicateQualityExtract(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
 		}
 	}
 }
